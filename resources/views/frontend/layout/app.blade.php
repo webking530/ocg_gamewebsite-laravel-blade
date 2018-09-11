795283417b7a4b54a5383ef37c8f5a366e847f40
alexplay03@gmail.com <al10ex29>
2018-09-10 22:23:14 -0400

    Added favicon

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -11,8 +11,8 @@
     <meta name="author" content="OCG - Online Casino Games">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
 
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
