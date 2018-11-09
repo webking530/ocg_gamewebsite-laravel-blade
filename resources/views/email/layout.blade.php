d30a5355031d9f65972d41e812a84f8eb0cff7fb
alexplay <al10ex29>
2018-11-09 13:02:12 -0400

    Fixed bug in email layout

--- resources/views/email/layout.blade.php
+++ resources/views/email/layout.blade.php
@@ -13,7 +13,7 @@
     <div style="width:450px;margin:auto;background:white;padding:10px;text-align:center">
         <hr>
         <p><em>{{ trans('emails.email_automatic') }}</em></p>
-        <p><a target="_blank" href="{{ route('home.terms') }}">{{ trans('legal.terms_conditions') }}</a> | <a target="_blank" href="{{ route('home.policy') }}">{{ trans('legal.privacy_policy') }}</a></p>
+        <p><a target="_blank" href="{{ route('home.terms') }}">{{ trans('legal.terms_conditions.title') }}</a> | <a target="_blank" href="{{ route('home.policy') }}">{{ trans('legal.privacy_policy.title') }}</a></p>
         <p><a href="{{ route('home') }}">{{ trans('app.meta.short_title') }}</a> |
             <a href="https://plus.google.com/u/0/101876203649115488412">Google+</a> |
             <a href="https://twitter.com/OCGcasino">Twitter</a> |
