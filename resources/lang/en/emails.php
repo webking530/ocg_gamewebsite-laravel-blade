528cc3fd2fcae807bf5b546e616053a413ce217a
alexplay <al10ex29>
2018-11-13 12:16:15 -0400

    Changed link color for email

--- resources/lang/en/emails.php
+++ resources/lang/en/emails.php
@@ -5,7 +5,7 @@ return [
     'activation' => [
         'subject' => 'Your verification PIN',
         'body_1' => 'Hello :name, in order to access your account, you must first activate it using the following PIN:',
-        'body_2' => 'Visit the following link: <a href=":route">Activate</a>, to enter your PIN.'
+        'body_2' => 'Visit the following link: <a style="color:#85a8e6" href=":route">Activate</a>, to enter your PIN.'
     ],
 
     'email_error' => 'The email could not be sent at this moment. Please try again later.',
