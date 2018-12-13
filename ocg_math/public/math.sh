d8ce73b267a88d50466211800f03bac5c1220339
alexplay <al10ex29>
2018-12-12 22:27:57 -0400

    Modified .bat to .sh

---
+++ ocg_math/public/math.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nodejs math.js TheFruits
+nodejs math.js SpaceAdventure BonusItem3 BonusItem4 BonusItem5
+nodejs math.js ArabianNights ArabianNightsFreeSpins BonusWheel3 BonusWheel4 BonusWheel5 FreeSpins
---
+++ ocg_math/public/server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nodejs server.js
